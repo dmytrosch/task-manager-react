54651211168e77d760c1de4eae3b2ee98bc22ff5
elena-salam <elena.salamakha.ua@gmail.com>
2021-02-10 22:57:06 +0200

    three orange Buttons

---
+++ src/common/Button/index.js
@@ -0,0 +1,27 @@
+import React from "react";
+import PropTypes from "prop-types";
+import classNames from "classnames";
+import styles from "./Button.module.css";
+
+const Button = ({ color, shape, children, buttonCustomClass, ...props }) => {
+  return (
+    <button
+      className={classNames(styles.button, styles[color], styles[shape], ...[buttonCustomClass])}
+      {...props}
+    >
+      {children}
+    </button>
+  );
+};
+
+Button.proprTypes = {
+  color: PropTypes.string,
+  shape: PropTypes.string,
+};
+
+Button.defaultProps = {
+  color: "orange",
+  shape: "square",
+};
+
+export default Button;
