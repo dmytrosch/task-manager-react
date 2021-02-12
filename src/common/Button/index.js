dbc9f2ffcd4c7543bfeabb2bb03fcbe639767b7c
Maksym Kovalenko <m.v.kovalenko4@gmail.com>
2021-02-13 01:31:39 +0200

    styled btn component

--- src/common/Button/index.js
+++ src/common/Button/index.js
@@ -1,27 +1,32 @@
-import React from "react";
-import PropTypes from "prop-types";
-import classNames from "classnames";
-import styles from "./Button.module.css";
+import React from 'react'
+import PropTypes from 'prop-types'
+import classNames from 'classnames'
+import styles from './Button.module.css'
 
 const Button = ({ color, shape, children, buttonCustomClass, ...props }) => {
   return (
     <button
-      className={classNames(styles.button, styles[color], styles[shape], ...[buttonCustomClass])}
+      className={classNames(
+        styles.button,
+        styles[color],
+        styles[shape],
+        ...[buttonCustomClass]
+      )}
       {...props}
     >
       {children}
     </button>
-  );
-};
+  )
+}
 
 Button.proprTypes = {
   color: PropTypes.string,
   shape: PropTypes.string,
-};
+}
 
 Button.defaultProps = {
-  color: "orange",
-  shape: "square",
-};
+  color: 'orange',
+  shape: 'square',
+}
 
-export default Button;
+export default Button
--- src/components/Layouts/MainLayout.js
+++ src/components/Layouts/MainLayout.js
@@ -1,13 +1,14 @@
 import React from 'react'
 import styles from './Layout.module.css'
 import Header from '../Header/Header'
-import Sidebar from '../SideBar/SideBar'
+import SideBar from '../SideBar/SideBar'
 
 export default function MainLayout(props) {
   return (
     <div className={styles.wrapper}>
       <div className={styles.containerHeader}>
         <Header />
+        <SideBar />
       </div>
       <div className={styles.container}>{props.children}</div>
     </div>
