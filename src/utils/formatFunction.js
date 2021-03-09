f5aeb29cc176aa2bda3dd6579fa47a0f33c5afb5
Oleg <kolyushko.oleg@gmail.com>
2021-03-09 22:54:17 +0200

    fixed

--- src/utils/formatFunction.js
+++ src/utils/formatFunction.js
@@ -1,46 +1,21 @@
+const monthNameMap = {
+  1: "Січ",
+  2: "Лют",
+  3: "Бер",
+  4: "Квіт",
+  5: "Трав",
+  6: "Черв",
+  7: "Лип",
+  8: "Серп",
+  9: "Вер",
+  10: "Жовт",
+  11: "Лист",
+  12: "Груд",
+}
+
 export function formatDate(date) {
   const [year, month, day] = date.split("/");
-  let monthName;
-  switch (month) {
-    case "1":
-      monthName = "Січ";
-      break;
-    case "2":
-      monthName = "Лют";
-      break;
-    case "3":
-      monthName = "Бер";
-      break;
-    case "4":
-      monthName = "Квіт";
-      break;
-    case "5":
-      monthName = "Трав";
-      break;
-    case "6":
-      monthName = "Черв";
-      break;
-    case "7":
-      monthName = "Лип";
-      break;
-    case "8":
-      monthName = "Серп";
-      break;
-    case "9":
-      monthName = "Вер";
-      break;
-    case "10":
-      monthName = "Жовт";
-      break;
-    case "11":
-      monthName = "Лист";
-      break;
-    case "12":
-      monthName = "Груд";
-      break;
-    default:
-      monthName = "";
-  }
+  const monthName = monthNameMap[month];
   const formatData = `${day} ${monthName}`;
   return formatData;
 }
